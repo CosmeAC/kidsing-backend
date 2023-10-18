4804bb3a12e8eaaeb7611cbfc51493afc7c3b50b
alex <acosme1988@gmail.com>
2023-10-18 21:25:54 +0200

    Reedme inst y info + codigo backend

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # kidsing-backend
+
+# Instalacion - npm init
+Para poder utilizar el código necesitas las siguientes instalaciones:
+For use the code need the next instalations:
+
+npm i
+or
+npm i auth0 axios bcrypt body-parser cors dotenv express mysql
+
+
+# Carpetas
+-CONFIG
+En la carpeta config está la información de la base de datos local (mirar los campos por si necesitan modificación).
+Config is where you can see all the information about local database - see the information maybe you should do some changes -
+
+-DUMP
+En la carpeta dump está el archivo mysql, puedes encontrar la información para crear una nueva basededatos si lo necesitas.
+The folder dump is where you can find the mysql file and all the information about the database. if u dont have it u can use to create one.
+
+# Backend Code 
+PROCEDIMIENTO 
+
+Las instalaciones se usan para lo siguiente:
+
+-auth0: paquete para usar un login de la propia compañía Auth0 que permite entrar con Google
+-axios: paquete facilita el código
+-bcrypt: se utiliza para poder usar la codificación que jsw de Auth0
+-body-parser: para poder leer los datos que se envían del frontend al backend
+-cors: paquete facilita el código
+-dotenv: para guardar información personal/contraseñas/configuracion...
+-express: para poder conectar el servidor y gestionarlo
+-mysql: para poder conectar a la base de datos mysql
+
+1º- Se crea una base de datos mysql para poder hacer la conexión
+2º- Como la intención es modular el código para tener mejor organización hice un archivo donde se crea la conexión a la base de datos, de
+esta forma se nos ahorramos utilizar este código que usaríamos muchas veces accesible por si necesitamos modificar el código.
+3º- procedemos a crear la conexión con Auth0:
+    -En la página web se crea un usuario y un servicio de login que proporciona el dominio, la contraseña y el ID. Ponemos esta información el archivo .env
+    -Crearemos una ruta get para poder enviar esta información al frontend donde la recibirá para poder entrar al login
+    -Junto a esta crearemos una ruta post para poder usar esta informacion del usuario
+4º- Funcionando la conexión al servicio de Auth0 crearemos otra API para los jugadores, de esta forma podremos tener varios jugadores en la misma cuenta de usuario.
+Esto se hará con la información del usuario registrado en la API del usuario, con esta información crearemos rutas para la creación de un jugador y mostrar todos los jugadores. Mientras hacía las API de usuarios y jugadores, me creaba una frontend muy básico para poder verificar la funcionalidad. Como tenemos claro el método y la tecnologías que usaríamos, utilice el React de la forma que usábamos en el proyecto
+
+5º- Una vez solucionando los errores y comprobando la funcionalidad. Procedo a ayudar al frontend donde hay mucha más carga de trabajo
+
+en el frontend procedere a hacer las páginas relacionadas con el backend, facilitando la implementación el código bakend con el frontend y arreglando y poniendo bonito las rutas de ver jugadores y crear jugadores que ya había hecho
+También me dedicaré a unificar el código(frontend-data-backend) y crearé rutas para los componentes que estamos creando
---
+++ backend/server.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 5000;
+const dbConn = require('./config/db.config');
+
+app.use(cors());
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true })) // parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.json());
+
+// ---enviar datos del dominio de auth0 
+app.get('/login', (req, res) => {
+  console.log('Recibida solicitud en /login');
+  const auth0Config = {
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET : process.env.AUTH0_CLIENT_SECRET,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+  };
+  res.json(auth0Config);
+});
+
+
+//--- envia los datos del usuario
+app.post('/perfil', (req, res) => {
+console.log('Solicitud POST recibida en /perfil');
+const { id, username, email } = req.body;
+const values = [id, username, email];
+
+const sql = 'INSERT INTO users (auth0_id, username, email) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE username = VALUES(username), email = VALUES(email)';
+dbConn.query(sql, values, (err, result) => {
+  if (err) {
+    console.log("error: ", err);
+    res.status(500).send("Error en la inserción o actualización de datos.");
+  } else {
+    if (result.affectedRows > 0) {
+      // Se insertó un nuevo registro o se actualizó uno existente
+      console.log("Usuario insertado o actualizado con éxito.");
+      res.status(200).send("Usuario insertado o actualizado con éxito.");
+    } else {
+      // No se insertó ni se actualizó ningún registro (tal vez no había cambios)
+      console.log("Ningún cambio en la base de datos.");
+      res.status(200).send("Ningún cambio en la base de datos.");
+    }
+  }
+});
+});
+
+// --- Crear un jugador
+app.post('/CrearJugador', (req, res) => {
+  const { nombre, userId, imagen } = req.body;
+  // const imagenBuffer = req.file.buffer; // la imagen esta por serparado por el cambio de formato a binario
+
+  const sql = 'INSERT INTO players (username, auth0_id, imagen) VALUES (?, ?, ?)';
+  const values = [nombre, userId,imagen]; // Utiliza el auth0_id como valor
+
+  // Ejecuta la consulta SQL para insertar el jugador
+  dbConn.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error al insertar jugador en MySQL: ', err);
+      res.status(500).json({ error: 'Error al insertar jugador' });
+      return;
+    }else {
+      // No se insertó ni se actualizó ningún registro (tal vez no había cambios)
+      console.log("Jugador insertado");
+         // ID del jugador insertado
+    const jugadorId = result.insertId;
+    
+    // Responde con el ID del jugador insertado
+    res.status(200).json({ jugadorId });
+    }
+  });
+});
+
+// ---Enviar datos del jugadora  frontend
+app.get('/MostrarJugadors/:auth0_id', (req, res) => {
+  const auth0Id = req.params.auth0_id; // user id del auth0 del usuario que esta en la web
+
+  const sql = 'SELECT username, imagen FROM players WHERE auth0_id = ?';
+  const values = [auth0Id]; // Utiliza el auth0_id como valor
+
+  // const sql = 'INSERT INTO players (username) VALUES (?)';
+  // const values = [nombre ]; // Utiliza el auth0_id como valor
+
+  dbConn.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error al enviar jugador a Frontend: ', err);
+      res.status(500).json({ error: 'Error al enviar jugador a Frontend' });
+      return;
+    }else {
+      // No se insertó ni se actualizó ningún registro (tal vez no había cambios)
+      console.log("Api Jugador enviado");
+         // ID del jugador insertado
+    const jugadorId = result;
+    
+    // Responde con el ID del jugador insertado
+    res.status(200).json({ jugadorId });
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send( 'Api conectad')
+});
+
+app.post('/', (req, res) => {
+  res.send('datos guardado')
+});
+
+// app.get('/protected', async (req, res) => { //sirve para ver los datos del usuario autenticado
+//   try {
+//     const authorizationHeader = req.headers.authorization;
+    
+//     if (!authorizationHeader) {
+//       return res.status(401).json({ message: 'Acceso no autorizado. Token de acceso no proporcionado.' });
+//     }
+    
+//     // Dividir el encabezado de autorización en base al espacio
+//     const parts = authorizationHeader.split(' ');
+
+//     if (parts.length !== 2 || parts[0] !== 'Bearer') {
+//       return res.status(401).json({ message: 'Acceso no autorizado. Token de acceso no válido.' });
+//     }
+
+//     const accessToken = parts[1];
+
+//     // Ahora que tenemos el token, podemos hacer la solicitud a la URL de userinfo de Auth0
+//     const response = await axios.get('https://dev-lgz8outtbace26ob.us.auth0.com/userinfo', {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//     });
+
+//     const userinfo = response.data;
+
+//     // Aquí puedes hacer lo que necesites con la información del usuario
+//     // userinfo contiene los datos del usuario autenticado
+
+//     res.json(userinfo);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error interno del servidor' });
+//   }
+// });
+
+// ---escuchar el puerto
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+}); 
